7679c798527ddb8128d209405bda2b6887372ce5
aishaohua <shaohua.ai@ecarx.com.cn>
2020-07-27 14:57:40 +0800

    second

--- app/src/main/java/com/archermind/kotlinplayer/model/TestPresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.archermind.kotlinplayer.model
-
-/**
- * Created by 123 on 2020/7/27.
- */
-class TestPresenter {
-}
--- app/src/main/java/com/archermind/kotlinplayer/ui/fragment/TestPresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.archermind.kotlinplayer.ui.fragment
-
-/**
- * Created by 123 on 2020/7/27.
- */
-class TestPresenter {
-}
