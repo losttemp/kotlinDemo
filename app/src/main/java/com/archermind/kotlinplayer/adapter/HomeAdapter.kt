3e0585fd31e97b84d83e81ac0dbbff95abdbcc3b
aishaohua <shaohua.ai@ecarx.com.cn>
2020-08-03 10:26:41 +0800

    完成下拉刷新和上拉加载更多

--- app/src/main/java/com/archermind/kotlinplayer/adapter/HomeAdapter.kt
+++ app/src/main/java/com/archermind/kotlinplayer/adapter/HomeAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.archermind.kotlinplayer.widget.HomeItemView
+import com.archermind.kotlinplayer.widget.LoadMoreView
 import com.itheima.player.model.bean.HomeItemBean
 
 class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
@@ -11,11 +12,26 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): HomeHolder {
-        return HomeHolder(HomeItemView(parent?.context))
+        if (viewType == 1) {
+            return HomeHolder(HomeItemView(parent?.context))
+        } else {
+            return HomeHolder(LoadMoreView(parent?.context))
+        }
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == list.size) {
+            //最后一条
+            return 1
+        } else {
+            //普通条目
+            return 0
+        }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return list.size + 1
     }
 
     override fun onBindViewHolder(holder: HomeHolder?, position: Int) {
@@ -29,6 +45,13 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeHolder>() {
         this.list.addAll(updatalist!!)
         notifyDataSetChanged()
 
+    }
+
+    fun loadMore(list: List<HomeItemBean>?) {
+        list?.let {
+            this.list.addAll(it)
+            notifyDataSetChanged()
+        }
     }
 
     class HomeHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/archermind/kotlinplayer/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/archermind/kotlinplayer/ui/fragment/HomeFragment.kt
@@ -1,6 +1,9 @@
 package com.archermind.kotlinplayer.ui.fragment
 
+import android.graphics.Color
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import com.archermind.kotlinplayer.R
@@ -25,7 +28,66 @@ class HomeFragment : BaseFragment() {
 
     override fun initListener() {
         recycleView.layoutManager = LinearLayoutManager(context)
+        refreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GRAY)
+        //下拉刷新
+        refreshLayout.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
+            override fun onRefresh() {
+                initDatas()
+            }
+
+        })
+        recycleView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    val layoutManager = recyclerView?.layoutManager
+                    if (layoutManager is LinearLayoutManager) {
+                        //val manager: LinearLayoutManager = layoutManager
+                        val LastVisiblePosition = layoutManager.findLastVisibleItemPosition()
+                        if (LastVisiblePosition == homeAdapter.itemCount - 1) {
+                            loadMore(homeAdapter.itemCount - 1)
+                        }
+                    }
+                }
+            }
+        })
     }
+
+    private fun loadMore(offset: Int) {
+        val path = URLProviderUtils.getHomeUrl(offset, 20)
+        val okHttpClient = OkHttpClient()
+        val request = Request.Builder()
+                .url(path)
+                .get()
+                .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                showToast("获取数据失败")
+
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val result = response?.body()?.string()
+                Log.d("result", "result=" + result)
+                val gson = Gson()
+                //接口失效
+                val list = gson.fromJson<List<HomeItemBean>>(result, object : TypeToken<List<HomeItemBean>>() {}.type)
+                //刷新列表
+                ThreadUtil.runOnMainThread(object : Runnable {
+                    override fun run() {
+                        //加载更多
+                        homeAdapter.loadMore(list)
+                    }
+
+                })
+            }
+
+        })
+    }
+
 
     override fun initData() {
 
@@ -35,7 +97,6 @@ class HomeFragment : BaseFragment() {
 
     private fun initDatas() {
         val path = URLProviderUtils.getHomeUrl(0, 20)
-        Log.d("path===", path)
         val okHttpClient = OkHttpClient()
         val request = Request.Builder()
                 .url(path)
